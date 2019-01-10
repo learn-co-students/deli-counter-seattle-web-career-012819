055b2e1673f3bfa0ecd1bc952f61aa6ce904ccb9
Nat-Reid <nathanielreid@gmail.com>
2019-01-09 19:20:45 -0800

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+# Write your codeharfwe
+
+def line(line)
+  puts "The line is currently" + ((line.empty?) ? (" empty.") : (":" + line.map.with_index{|name, number|" #{number+1}. #{name}"}.join))
+end
+
+def take_a_number(line, name)
+  puts "Welcome, #{line.push(name).last}. You are number #{line.size} in line."
+end
+
+def now_serving(line)
+  puts ((line.empty?) ? ("There is nobody waiting to be served!") : ("Currently serving #{line.shift}."))
+end
