5a753477b7d575ad38c73fd935139bef5924d444
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 00:37:07 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+def line (array)
+  if array.size == 0
+    puts "The line is currently empty"
+  else
+    puts "The line is currently: "
+
+    index = 1;
+
+    array.each do |name|
+      print "#{index}. #{name} "
+      index += 1
+    end
+  end
+end
